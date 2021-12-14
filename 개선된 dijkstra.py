aa8f2069181a8b16203004a64735136e12d148ab
chnlee <84317436+chnlee@users.noreply.github.com>
2021-12-14 22:05:35 +0900

    Add files via upload

--- 개선된 dijkstra.py
+++ 개선된 dijkstra.py
@@ -1,36 +1,44 @@
 import heapq
 import sys
 input = sys.stdin.readline
-INF = int(1e9)
-
+INF = int(1e9) # 무한을 의미하는 값을 10억으로 설정한다.
+# 노드 및 간선의 개수를 입력받는다.
 n, m = map(int,input().split())
-
+# 시작 노드의 번호를 입력받는다.
 start = int(input())
-
+# 노드에 대한 정보를 담는 리스트 생성
 graph = [[] for i in range(n+1)]
-
+# 최단 거리 테이블을 모두 무한으로 초기화한다.
 distance = [INF] * (n+1)
 
-
+# 모든 간선 정보를 입력받는다.
 for _ in range(m):
   a,b,c = map(int,input().split())
+  # a 번 노드에서 b번 노드로 가는 비용이 c라는 의미이다.
   graph[a].append(b,c)
 
 
 def dijkstra(start):
   q = []
+  # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입한다. (우선순위 큐에 대한 데이터 삽입)
   heapq.heappush(q,(0,start))
   distance[start] = 0
+  # 큐가 비어있지 않다면
   while q:
+    # 가장 최단 거리가 짧은 노드에 대한 정보를 꺼낸다.
     dist,now = heapq.heappop(q)
-    if distance[now] > dist:
+    # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+    if distance[now] < dist:
       continue
+    # 현재 노드와 연결된 다른 인접한 노드를 확인한다.
     for i in graph[now]:
       cost = dist + i[1]
+      # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우 distance를 변경한다.
       if cost < distance[i[0]]:
         distance[i[0]] = cost
         heapq.heappush(q,cost(i[0]))
 
+# 다익스트라 알고리즘을 수행한다.
 dijkstra(start)
 
 for i in range(1, n+1):
@@ -38,3 +46,6 @@ for i in range(1, n+1):
     print("INFINITY")
   else:
     print(distance[i])
+
+ 
+# 시작 노드로 가기 위한 최단 경로를 0으로 설정하기.
