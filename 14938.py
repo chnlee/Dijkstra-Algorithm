0c4159bd2f7bd3b0cfbf21bc62b60b4b531c6941
chnlee <84317436+chnlee@users.noreply.github.com>
2022-01-03 13:54:22 +0900

    Add files via upload

--- 14938.py
+++ 14938.py
@@ -2,7 +2,6 @@ import heapq
 INF = float('inf')
 n, m, r = map(int,input().split())
 item = list(map(int,input().split()))
-distance = [INF] * (n+1)
 graph = [[] for i in range(n+1)]
 
 for i in range(r):
@@ -12,6 +11,8 @@ for i in range(r):
 
 
 def djikstra(start):
+  result = 0
+  distance = [INF] * (n+1)
   q = []
   heapq.heappush(q,(0,start))
   distance[start] = 0
@@ -24,16 +25,16 @@ def djikstra(start):
       if cost < distance[i[0]]:
         distance[i[0]] = cost
         heapq.heappush(q,(cost,i[0]))
-    
+  for i in range(1,n+1):
+    if distance[i] <= m:
+      result += item[i-1]
+  return result 
 
-djikstra(r)
-
-result = 0
-for i in range(1,n+1):
-  print(distance[i])
-  if distance[i] < m:
-    result += item[i-1]
+value = 0
+for k in range(1, n+1):   
+  value = max(value, djikstra(k))
   
 
-print(result)
+
+print(value)
   
